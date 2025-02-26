d6b8654b4cbc29cd12b54562a19391db9053cc7f
agene-thai <thai.huynh@agene.io>
2025-02-26 14:26:17 +0700

    add google analytic

--- src/App.tsx
+++ src/App.tsx
@@ -140,7 +140,7 @@ function MapComponent() {
             setPlaceId={setPlaceId} 
           />
         </div>
-        <button
+      <button
           onClick={() => setIsConfirming(true)}
           className="mt-4 px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 cursor-pointer"
         >
